411915b8294a9640203ea2251b5d0d426592ab1f
ROHIT SHARMA <rohit.sharma@shivila.com>
2022-06-03 10:51:59 +0530

    Initial commit

---
+++ app/src/main/java/com/dabbaservices/shopping/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.dabbaservices.shopping.Prevalent;
+
+import com.dabbaservices.shopping.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
